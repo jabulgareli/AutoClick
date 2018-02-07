684e32527c86284a56632069c4e6ae937e247dfb
LINX-INVES\joao.bulgareli <joao.bulgareli@linx.com.br>
2018-02-07 10:15:11 -0200

    Add project files.

---
+++ AutoClick/FrmMain.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace AutoClick
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.threadClick = new System.ComponentModel.BackgroundWorker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // threadClick
+            // 
+            this.threadClick.DoWork += new System.ComponentModel.DoWorkEventHandler(this.threadClick_DoWork);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(2, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(361, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "F2 - Start/Stop";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(2, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(361, 32);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Stopped";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(364, 105);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Jab - AutoClick";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker threadClick;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ AutoClick/FrmMain.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace AutoClick
+{
+    public partial class Form1 : Form
+    {
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+
+        public const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        public const int MOUSEEVENTF_LEFTUP = 0x04;
+
+        public void LeftMouseClick(int xpos, int ypos)
+        {
+            mouse_event(MOUSEEVENTF_LEFTDOWN, xpos, ypos, 0, 0);
+            mouse_event(MOUSEEVENTF_LEFTUP, xpos, ypos, 0, 0);
+        }
+
+        private bool doClick = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void threadClick_DoWork(object sender, DoWorkEventArgs e)
+        {
+            while (doClick)
+            {
+                LeftMouseClick(Cursor.Position.X, Cursor.Position.Y);
+                Thread.Sleep(100);
+                e.Cancel = true;
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.F2)
+            {
+                doClick = !doClick;
+                if (doClick)
+                {
+                    threadClick.RunWorkerAsync();
+                    label2.Text = "Started";
+                    label2.ForeColor = Color.Green;
+                }
+                else
+                {
+                    while (threadClick.IsBusy)
+                    {
+                        label2.Text = "Stopping";
+                        label2.ForeColor = Color.Yellow;
+                        Application.DoEvents();
+                        continue;
+                    }
+
+                    label2.Text = "Stopped";
+                    label2.ForeColor = Color.Red;
+
+                }
+            }
+        }
+    }
+}
